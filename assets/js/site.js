8ea6fc52dc3e1933d8b70c0fa78b47122054420c
styll2025 <Saraportell@gmail.com>
2026-07-07 21:52:54 +0100

    Add files via upload

---
+++ assets/js/site.js
@@ -0,0 +1,6 @@
+function toggleMobileMenu() {
+  document.getElementById('mobile-menu').classList.toggle('is-open');
+}
+function closeMobileMenu() {
+  document.getElementById('mobile-menu').classList.remove('is-open');
+}
